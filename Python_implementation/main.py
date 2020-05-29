4e4832df7202a25bc824af02bb42846fb780db76
tealflipper <tealflipper@gmail.com>
2020-05-29 01:50:18 -0500

    r2d2 branch

---
+++ Python_implementation/main.py
@@ -0,0 +1,17 @@
+#libraries included
+"""
+para crear nuevo hilo
+
+"""
+import threading #lo mismo que pthread
+
+#Global scope variables
+
+#Data structures
+
+#opens threads for consumers
+def Consumers (threading.Thread):
+  def __init__(self,threadID,si): #id de hilo y su conjunto si
+    threading.Thread.__init__(self)
+
+#Functions
