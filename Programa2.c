89bc1a46012666bdeeb26ca1cbac8f7f2910a6c5
tealflipper <tealflipper@gmail.com>
2020-05-29 01:47:47 -0500

    Sirven productor-consumidor, falta realizar operaciones de matriz

--- Programa2.c
+++ Programa2.c
@@ -4,81 +4,90 @@
 #include <semaphore.h>
 #include <stdlib.h>
 #include <math.h>
+#include "Queue.h"
 
 #define SIZE_UNIVERSO 4
 //Prototipos de funciones
-int calcularScore(int* s , char** adn, int tamMotivo, int numCadenasADN);
-int* encontrarMotivos(char** adn,int tamMotivo, int numCadenasADN); //l longitud patron oculto regresa el mejor s
+int calcularScore(int* s , int** matrizPerfiles);
+int* encontrarMotivos(char** adn, int numCadenasADN); //l longitud patron oculto regresa el mejor s
 void imprimirS(int* a, int t, int start, int n); //arreglo que se usara, numero de cadenas, start es 0, n es la longitud de la cadena
+void getVectorS(int* a, int t, int start, int n);
+void Consummer();
 
 //Variables globales
 int tamADN;
+int tamMotivo;
+int numCadenasADN;
 char** cadenasADN;
-int** matrizPerfiles;
-int* perfilObtenido;
-
+int* perfilObtenido;//mejor conjunto de S
+int hechosP=0,hechosC,totales;
+sem_t empty,full,mutex;
 
 
 //Variables compartidas
 int bestScore;
 
-
-
-int main(){
+int main(int argc, char const *argv[]){
+  //abrir archivo
   FILE *fichero;
-  fichero = fopen("data.txt", "r");
+  int l;
+  char* motivo; 
+  int n;
+  int t;
+  int tamBuffer=10;
+  sem_init(&empty,0,tamBuffer);
+  sem_init(&full,0,1);
+  sem_init(&mutex,0,1);
+  
+  if(argc>1) fichero = fopen(argv[1], "r");
+  else fichero = fopen("data.txt", "r");
   //TAM MOTIVO
-  int tamMotivo; fscanf(fichero, "%d", &tamMotivo); printf("Tam Motivo(L): %d\n",tamMotivo);
-  int l=tamMotivo;
-  //MOTIVO
-  char* motivo; motivo = (char*)malloc(tamMotivo*sizeof(char));
-  fscanf(fichero, "%s",motivo); printf("Motivo: %s\n",motivo);
-
-  //TAM CADENA DE ADN
-  fscanf(fichero, "%d", &tamADN); printf("Tam ADN(n): %d\n",tamADN);
-
+  fscanf(fichero, "%d", &tamMotivo);
+  motivo = (char*)malloc(tamMotivo*sizeof(char));
+  //MOTIVO este es el que se espera encontrar
+  fscanf(fichero, "%s",motivo); 
+  //TAM CADENA DE ADN 
+  fscanf(fichero, "%d", &tamADN); 
   //NUMERO DE CADENAS DE ADN (RENGLONES)
-  int numCadenasADN = 0;
   fscanf(fichero, "%d", &numCadenasADN); printf("Numero cadenas ADN(t): %d\n",numCadenasADN);
-
   //LECTURA DE RENGLONES DE ADN
   cadenasADN = (char**)malloc(numCadenasADN*sizeof(char*));
   for(int i=0; i < numCadenasADN; i++){ cadenasADN[i] =(char*) malloc(tamADN*sizeof(char));}
   for(int i = 0; i < numCadenasADN; i++ ){ fscanf(fichero, "%s",cadenasADN[i]);}
+
+  printf("Tam Motivo(L): %d\n",tamMotivo);
+  printf("Motivo: %s\n",motivo);
+  printf("Tam ADN(n): %d\n",tamADN);
+  printf("Cadenas de ADN\n");
   for(int i = 0; i < numCadenasADN; i++ ){ for(int j=0; j < tamADN; j++){ printf("%c",cadenasADN[i][j]);}printf("\n" );}
-
-  //Arreglo para maximos de perfilesAlineados
-  int* maximos;
-  maximos = calloc (tamMotivo ,sizeof(int));
-
-  // Matriz temporal para perfilesAlineados
-  matrizPerfiles = (int**) calloc (SIZE_UNIVERSO ,sizeof(int *));
-  for(int i=0; i < SIZE_UNIVERSO; i++){ matrizPerfiles[i] =(int*) calloc(tamMotivo,sizeof(int)); }
-
   //Matriz para los mejores resultados de S
-
   perfilObtenido = (int*) calloc (tamMotivo, sizeof(int)); //se inicializa en 0
+
+  l=tamMotivo;
+  n=tamADN;
+  t=numCadenasADN;
+  totales=pow ((tamADN - tamMotivo) +1 ,numCadenasADN); //(n-l+1)^t
+
   printf("----------------------------------------------------------\n" );
 
-  //COMIENZAN PRUEBAS
-  int* S;
-  S = (int*) malloc(numCadenasADN*sizeof(int));
-  S[0] = 0;
-  S[1] = 0;
-  S[2] = 0;
-  printf("\nImprimimos S\n" );
-  imprimirS(S,numCadenasADN,0,3);// el ultimo valor tiene que ser n-l+1
-  printf("\n\n" );
 
-  bestScore = calcularScore(S ,cadenasADN,tamMotivo,numCadenasADN);
-  printf("\nPuntaje \n%d\n",bestScore );
-  //S = encontrarMotivos(cadenasADN,tamMotivo);
+  
+  int* S=(int*)calloc(t,sizeof(int));
+  //Empieza productor
+  //termina productor
+  pthread_t cons[4];
+  for(int i=0;i<4;i++) pthread_create(&cons[i], NULL,(void*)&Consummer,NULL);
+  getVectorS(S,t,0,n-l+1);
+  for(int i=0;i<4;i++) pthread_join(cons[i], NULL);
 
+  sem_destroy(&empty);
+  sem_destroy(&full);
+  sem_destroy(&mutex);
 
       return 0;
 }
 
-int* encontrarMotivos(char** adn,int tamMotivo, int numCadenasADN){
+int* encontrarMotivos(char** adn, int numCadenasADN){
   int* s;
   int acarreo = 1;
   int posicion = numCadenasADN - 1;
@@ -91,24 +100,50 @@ int* encontrarMotivos(char** adn,int tamMotivo, int numCadenasADN){
 return s;
 }
 
+void getVectorS(int* a, int t, int start, int n){
+  if(start==t){
+    //entra para insertar a cola
+    //Decide primero si termina
+    if(hechosP!=totales){
+      hechosP++;
+      sem_wait(&empty);
+      sem_wait(&mutex);
+      for(int i=0;i<t;i++) printf("%d", a[i]);
+        printf("\n");
 
+      sem_post(&mutex);
+      sem_post(&full);
+      //sale de cola
+    }
+     
+
+    
+  }
+  else{
+    for(int i=0; i<n; i++){
+      a[start]=i;
+      getVectorS(a, t, start+1,n);
+    }
+  }
+}
 
 void imprimirS(int* a, int t, int start, int n){
-    if(start==t){
-        for(int i=0;i<t;i++) printf("%d ", a[i]);
-        printf("\n");
-    }
-    else{
-        for(int i=0; i<n; i++){
+  if(start==t){
 
-            a[start]=i;
-            imprimirS(a, t, start+1,n);
-        }
+    for(int i=0;i<t;i++) printf("%d", a[i]);
+      printf("\n");
+
+  }
+  else{
+    for(int i=0; i<n; i++){
+      a[start]=i;
+      imprimirS(a, t, start+1,n);
     }
+  }
 }
 
 
-int calcularScore(int* s,char** cadenasADN, int tamMotivo, int numCadenasADN){ //en s vienen los numeros
+int calcularScore(int* s, int** matrizPerfiles){ //en s vienen los numeros
   int inicioExtraccion = 0;
   for(int i = 0; i < numCadenasADN; i++){
     inicioExtraccion = s[i];
@@ -131,23 +166,59 @@ int calcularScore(int* s,char** cadenasADN, int tamMotivo, int numCadenasADN){ /
   }
   printf("\n" );
 
-//Sumar valores para obtener el maximo de cada columna y sacar el score
-//printf("Valores maximos de cada columna\n");
-for(int i = 0; i < SIZE_UNIVERSO; i++){
-  for(int j = 0; j < tamMotivo; j++){
-          if(matrizPerfiles[i][j] > perfilObtenido[j]){
-              perfilObtenido[j] = matrizPerfiles[i][j];
-          }
+  //Sumar valores para obtener el maximo de cada columna y sacar el score
+  //printf("Valores maximos de cada columna\n");
+  for(int i = 0; i < SIZE_UNIVERSO; i++){
+    for(int j = 0; j < tamMotivo; j++){
+      //mutex lock
+      if(matrizPerfiles[i][j] > perfilObtenido[j]){
+      perfilObtenido[j] = matrizPerfiles[i][j];
+      }
+      //Mutex unlock
+    }
   }
-}
 
-printf("Extraccion de valores que mas repiten \n");
-int puntaje=0;
-for(int i = 0; i < tamMotivo; i++){
-  printf("%d ",perfilObtenido[i] );
-  puntaje+=perfilObtenido[i];
-}
-printf("\n" );
-
+  printf("Extraccion de valores que mas repiten \n");
+  int puntaje=0;
+  for(int i = 0; i < tamMotivo; i++){
+    printf("%d ",perfilObtenido[i] );
+    puntaje+=perfilObtenido[i];
+  }
+  printf("\n" );
   return puntaje;
+}
+
+
+void Consummer(){
+  sem_wait(&mutex);
+  while (hechosC!=totales){
+    hechosC++;
+    sem_post(&mutex);
+
+    sem_wait(&full);
+    sem_wait(&mutex);
+
+    sem_post(&mutex);
+    sem_post(&empty);
+  sem_wait(&mutex);
+  }
+  sem_post(&mutex);
+  /*
+  int** matrizPerfiles;
+    //Arreglo para maximos de perfilesAlineados
+  int* maximos = calloc (tamMotivo ,sizeof(int));
+  int* S;
+  // Matriz temporal para perfilesAlineados
+  matrizPerfiles = (int**) calloc (SIZE_UNIVERSO ,sizeof(int *));
+  for(int i=0; i < SIZE_UNIVERSO; i++){ matrizPerfiles[i] =(int*) calloc(tamMotivo,sizeof(int)); }
+ /*
+  * Entra a cola para sacar S
+  * 
+  
+
+  bestScore = calcularScore(S ,matrizPerfiles);
+  printf("\nPuntaje \n%d\n",bestScore );
+  S = encontrarMotivos(cadenasADN,tamMotivo);
+  
+*/
 }
--- Queue.h
+++ Queue.h
@@ -1,13 +1,26 @@
 typedef struct {
     int front, rear,capacity,elements;
-    int* q;
+    int** q;
 }Queue;
+
+int* qPeek(Queue q) {
+   return q.q[q.front];
+}
+int qIsEmpty(Queue q){
+    return q.elements==0;
+}
+int qIsFull(Queue q){
+    return q.elements==q.capacity;
+}
+int qSize(Queue q){
+    return q.elements;
+}
 
 Queue newQueue(int capacity){
     Queue new;
     new.front=new.rear=0;
     new.capacity=capacity;
-    new.q=(int*)calloc(capacity,sizeof(void));
+    new.q=(int**)calloc(capacity,sizeof(int*));
 }
 
 Queue deleteQueue(Queue toDelete){
@@ -23,8 +36,8 @@ void enqueue(Queue q, int* data){
         q.elements++;
     }
 }
-int dequeue(Queue q){
-    int data=q.q[q.front++];
+int* dequeue(Queue q){
+    int* data=(q.q[q.front++]);
     if(q.front==q.capacity){ q.front=0; }
     q.elements--;
     return data;
@@ -32,15 +45,3 @@ int dequeue(Queue q){
 
 
 
-int qPeek(Queue q) {
-   return q.q[q.front];
-}
-int qIsEmpty(Queue q){
-    return q.elements==0;
-}
-int qIsFull(Queue q){
-    return q.elements==q.capacity;
-}
-int qSize(Queue q){
-    return q.elements;
-}
